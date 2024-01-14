31dd5687ca32bad651a2a49a64feebd54062cf29
Harshit Bhutani <harshitbhutani22@gmail.com>
2024-01-15 00:17:34 +0530

    createUser error resolved

--- controller/Auth.js
+++ controller/Auth.js
@@ -5,28 +5,41 @@ const jwt = require('jsonwebtoken');
 
 
 exports.createUser = async (req, res) => {
-    const user = new User(req.body);
     try {
-        console.log('i am in jwt backend');
+        let user = new User(req.body);
+        console.log('i am in  create user');
+        console.log('user in create user coming, ', user);
         const salt = crypto.randomBytes(16);
-        crypto.pbkdf2(req.body.password, user.salt, 310000, 32, 'sha256', async function (err, hashedPassword) {            console.log('req body, ', ...req.body);
-            const user = new User({ ...req.body, password: hashedPassword, salt });
-            console.log('user', user);
-            const doc = await user.save();
+        crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', async function (err, hashedPassword) {
+            if (err) {
+                console.log('Error in hashing password:', err);
+                return res.status(500).json({ error: 'Error in hashing password' });
+            }
+            console.log('req body, ', req.body);
+            user = new User({ ...req.body, password: hashedPassword, salt });
+            console.log('this is the new user made ', user);
 
-            req.login(sanitizeUser(doc), (err) => {   // this also calls serializer and adds session
-                if (err) {
-                    res.status(400).json(err);
-                } else {
-                    console.log('token', token);
-                    const token = jwt.sign(sanitizeUser(doc), process.env.JWT_SECRET_KEY);
-                    res.cookie('jwt', token, { expires: new Date(Date.now() + 3600000), httpOnly: true }) // 1 hour session
-                    .status(201).json({id: doc.id, role: doc.role});
-                }
-            })
+            try {
+                const doc = await user.save();
+
+                req.login(sanitizeUser(doc), (err) => {   // this also calls serializer and adds session
+                    if (err) {
+                        console.log('we are in the error of create user');
+                        res.status(400).json(err);
+                    } else {
+                        console.log('user created successfully');
+                        const token = jwt.sign(sanitizeUser(doc), process.env.JWT_SECRET_KEY);
+                        res.cookie('jwt', token, { expires: new Date(Date.now() + 3600000), httpOnly: true }) // 1 hour session
+                            .status(201).json({ id: doc.id, role: doc.role });
+                    }
+                })
+            } catch (err) {
+                console.log('Error saving user:', err);
+                res.status(400).json(err);
+            }
         })
     } catch (err) {
-        console.log('here in error');
+        console.log('Error in try block:', err);
         res.status(400).json(err);
     }
 }
@@ -37,15 +50,15 @@ exports.loginUser = async (req, res) => {
     console.log('i am here in loginUser');
 
     res.cookie('jwt', req.user.token, { expires: new Date(Date.now() + 3600000), httpOnly: true, })
-       .status(201)
-       .json({id: user.id, role: user.role});
+        .status(201)
+        .json({ id: user.id, role: user.role });
 };
 
 exports.checkAuth = async (req, res) => {
-    if(req.user){
+    if (req.user) {
         console.log('i am in checkAuth node');
         res.json(req.user);
-    }else{
+    } else {
         console.log('i am in checkaurh error node');
         res.sendStatus(401);
     }
